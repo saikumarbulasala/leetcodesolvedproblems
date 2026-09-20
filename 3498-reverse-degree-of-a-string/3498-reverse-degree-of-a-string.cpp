d46054e00f93b06016f7507f39e13dc713ab07c0
Sai Kumar Bulasala <saikumarbulasala09@gmail.com>
2026-09-20 18:00:59 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.88%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n = s.size();
+        int revDeg = 0;
+        for(int i = 0;i<n;i++){
+            revDeg += ('z' - s[i] + 1) * (i + 1);
+        }
+
+        return revDeg;
+    }
+};
